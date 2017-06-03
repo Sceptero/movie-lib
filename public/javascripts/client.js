ee7bae78cb1daed6f13836c0a3aebf915fd93ea5
Sceptero <tir4misu@o2.pl>
2017-06-03 23:29:20 +0200

    worked on movie library views and web server communication with api

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,18 +3,17 @@ $(document).ready(() => {
   $('#registerButton').click(() => {
     const login = $('#loginInput').val();
     const password = $('#passwordInput').val();
-    
+
     $.ajax({
-      url: '/api/users',
+      url: '/register',
       method: 'POST',
       dataType: 'json',
-      data: { login: login, password: password },
-      success: (data, textStatus, jqXHR) => {
-        alert('Registered');
+      data: { login, password },
+      success: () => {
         window.location.replace('/login');
       },
-      error: (jqXHR, textStatus, errorThrown) => {
-        alert(jqXHR.responseJSON.message);
+      error: () => {
+        alert('registration error');
       },
     });
   });
@@ -25,32 +24,21 @@ $(document).ready(() => {
     const password = $('#passwordInput').val();
 
     $.ajax({
-      url: '/api/auth',
+      url: '/login',
       method: 'POST',
       dataType: 'json',
-      data: { login: login, password: password },
-      success: (data, textStatus, jqXHR) => {
-        console.log('sending internal req');
-        const recToken = data.token;
-        const recId = data.id;
-        const recLogin = data.login;
-
-        $.ajax({
-          url: '/login',
-          method: 'POST',
-          dataType: 'json',
-          data: { token: recToken, id: recId, login: recLogin },
-          success: () => {
-            window.location.replace('/');
-          },
-        });
+      data: { login, password },
+      success: () => {
+        window.location.replace('/');
       },
-      error: (jqXHR, textStatus, errorThrown) => {
-        alert(jqXHR.responseJSON.message);
+      error: () => {
+        alert('login error');
       },
     });
   });
 
+
+  // logout
   $('#logoutButton').click(() => {
     $.ajax({
       url: '/logout',
@@ -58,7 +46,31 @@ $(document).ready(() => {
       success: () => {
         window.location.replace('/login');
       },
+      error: () => {
+        alert('login error');
+      },
     });
   });
 
+  // add movie
+  $('#addMovieButton').click(() => {
+    const title = $('#movieTitle').val();
+    const rating = $('#movieRating').val();
+    const director = $('#movieDirector').val();
+    const actors = $('#movieActors').val();
+    const category = $('#movieCategory').val();
+
+    $.ajax({
+      url: '/add-movie',
+      method: 'POST',
+      dataType: 'json',
+      data: { title, rating, director, actors, category },
+      success: (data, textStatus, jqXHR) => {
+        window.location.replace('/');
+      },
+      error: (jqXHR, textStatus, errorThrown) => {
+        alert(jqXHR.responseJSON.message);
+      },
+    });
+  });
 });
--- routes/client.js
+++ routes/client.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const session = require('express-session');
+const axios = require('axios');
 
 const loginRequired = require('../middleware/session');
 const config = require('../config');
+const categories = require('../api/categories');
 
 const router = express.Router();
 
@@ -15,8 +17,73 @@ router.use(session({
 /**
  * Get home page.
  */
-router.get('/', loginRequired, (req, res) => {
-  res.render('index', { title: 'Home', login: req.login });
+router.get('/', loginRequired, async (req, res, next) => {
+  try {
+    const result = await axios(`http://localhost:3000/api/users/${req.session.user.id}/movies`, {
+      headers: {
+        Authorization: `Bearer ${req.session.user.token}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+
+    res.render('index', { title: 'Movie Library', login: req.login, movies: result.data });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/**
+ * Get new movie page.
+ */
+router.get('/add-movie', loginRequired, async (req, res, next) => {
+  res.render('addmovie', { title: 'Add Movie', login: req.login, categories });
+});
+
+/**
+ * New movie action.
+ */
+router.post('/add-movie', async (req, res, next) => {
+  try {
+    const result = await axios(`http://localhost:3000/api/users/${req.session.user.id}/movies`, {
+      method: 'post',
+      headers: {
+        Authorization: `Bearer ${req.session.user.token}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      data: {
+        title: req.body.title,
+        rating: req.body.rating,
+        director: req.body.director,
+        actors: req.body.actors,
+        category: req.body.category,
+      },
+    });
+
+    res.json({ message: 'Ok' });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/**
+ * Get single movie page.
+ */
+router.get('/movie/:id', loginRequired, async (req, res, next) => {
+  try {
+    const result = await axios(`http://localhost:3000/api/users/${req.session.user.id}/movies/${req.params.id}`, {
+      headers: {
+        Authorization: `Bearer ${req.session.user.token}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+
+    res.render('movie', { title: 'Edit Movie', login: req.login, movie: result.data });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 /**
@@ -26,6 +93,35 @@ router.get('/login', (req, res) => {
   if (req.session.user) res.redirect('/');
 
   res.render('login', { title: 'Login', login: req.login });
+});
+
+/**
+ * Login action.
+ */
+router.post('/login', async (req, res, next) => {
+  try {
+    const result = await axios('http://localhost:3000/api/auth', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      data: {
+        login: req.body.login,
+        password: req.body.password,
+      },
+    });
+
+    const sess = req.session;
+    sess.user = {};
+    sess.user.token = result.data.token;
+    sess.user.id = result.data.id;
+    sess.user.login = result.data.login;
+
+    res.json({ message: 'Ok' });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 
@@ -39,18 +135,28 @@ router.get('/register', (req, res) => {
 });
 
 /**
- * Login (create sesion).
+ * Register action.
  */
-router.post('/login', (req, res) => {
-  const sess = req.session;
+router.post('/register', async (req, res, next) => {
+  try {
+    const result = await axios('http://localhost:3000/api/users', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      data: {
+        login: req.body.login,
+        password: req.body.password,
+      },
+    });
 
-  sess.user = {};
-  sess.user.token = req.body.token;
-  sess.user.id = req.body.id;
-  sess.user.login = req.body.login;
-
-  res.json({ message: 'Ok' });
+    res.json({ message: 'Ok' });
+  } catch (err) {
+    return next(err);
+  }
 });
+
 
 /**
  * Logout (destroy session).
